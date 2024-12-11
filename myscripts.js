617a8481d86d992ff0608f8c973a56e0e37f6c9d
kasjanzalewski23 <kasjan.zalewski23@gmail.com>
2024-12-11 19:45:45 +0100

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,136 +1,134 @@
 // console.log("masz swój easter egg i spierdalaj!");
 
-// function winrate(winrate){
-//     console.log(typeof winrate);
-//     let str = document.getElementById(winrate).innerText;
-//     console.log(typeof winrate);
+function winrate(wr){
+    let str = document.getElementById("winrate" + wr).innerText;
+    let lastCh = str.charAt(str.length - 1);
+    if (lastCh != '%'){
+        let wins = document.getElementById("winrate" + wr).getAttribute("data-winrate");
+        document.getElementById("winrate" + wr).innerHTML = wins;
+    }else{
+        let nick = document.getElementById("winrate" + wr).getAttribute("data-nick");
+        document.getElementById("winrate" + wr).innerHTML = nick;
+    }
+}
+
+// function winrate1(){
+// console.log("ch");
+//     let str = document.getElementById("winrate1").innerText;
+//     let lastCh = str.charAt(str.length - 1);
+//     if (lastCh == '%' || str == "Not played yet"){
+//         document.getElementById("winrate1").innerHTML = document.getElementById("winrate1").getAttribute("data-nick");
+// console.log("1");
+//     }else{
+// 	document.getElementById("winrate1").innerHTML = document.getElementById("winrate1").getAttribute("data-winrate");
+// console.log("2");
+//     }
+// }
+
+// function winrate2(){
+//     let str = document.getElementById("winrate2").innerText;
 //     let lastCh = str.charAt(str.length - 1);
 //     if (lastCh != '%'){
-//         let wins = document.getElementById(winrate).getAttribute("data-winrate");
-//         document.getElementById(winrate).innerHTML = wins;
+//         let wins = document.getElementById("winrate2").getAttribute("data-winrate");
+//         document.getElementById("winrate2").innerHTML = wins;
 //     }else{
-//         let nick = document.getElementById(winrate).getAttribute("data-nick");
-//         document.getElementById(winrate).innerHTML = nick;
+//         let nick = document.getElementById("winrate2").getAttribute("data-nick");
+//         document.getElementById("winrate2").innerHTML = nick;
 //     }
-//}
+// }
 
-function winrate1(){
-console.log("ch");
-    let str = document.getElementById("winrate1").innerText;
-    let lastCh = str.charAt(str.length - 1);
-    if (lastCh == '%' || str == "Not played yet"){
-        document.getElementById("winrate1").innerHTML = document.getElementById("winrate1").getAttribute("data-nick");
-console.log("1");
-    }else{
-	document.getElementById("winrate1").innerHTML = document.getElementById("winrate1").getAttribute("data-winrate");
-console.log("2");
-    }
-}
+// function winrate3(){
+//     let str = document.getElementById("winrate3").innerText;
+//     let lastCh = str.charAt(str.length - 1);
+//     if (lastCh != '%'){
+//         let wins = document.getElementById("winrate3").getAttribute("data-winrate");
+//         document.getElementById("winrate3").innerHTML = wins;
+//     }else{
+//         let nick = document.getElementById("winrate3").getAttribute("data-nick");
+//         document.getElementById("winrate3").innerHTML = nick;
+//     }
+// }
 
-function winrate2(){
-    let str = document.getElementById("winrate2").innerText;
-    let lastCh = str.charAt(str.length - 1);
-    if (lastCh != '%'){
-        let wins = document.getElementById("winrate2").getAttribute("data-winrate");
-        document.getElementById("winrate2").innerHTML = wins;
-    }else{
-        let nick = document.getElementById("winrate2").getAttribute("data-nick");
-        document.getElementById("winrate2").innerHTML = nick;
-    }
-}
+// function winrate4(){
+//     let str = document.getElementById("winrate4").innerText;
+//     let lastCh = str.charAt(str.length - 1);
+//     if (lastCh != '%'){
+//         let wins = document.getElementById("winrate4").getAttribute("data-winrate");
+//         document.getElementById("winrate4").innerHTML = wins;
+//     }else{
+//         let nick = document.getElementById("winrate4").getAttribute("data-nick");
+//         document.getElementById("winrate4").innerHTML = nick;
+//     }
+// }
 
-function winrate3(){
-    let str = document.getElementById("winrate3").innerText;
-    let lastCh = str.charAt(str.length - 1);
-    if (lastCh != '%'){
-        let wins = document.getElementById("winrate3").getAttribute("data-winrate");
-        document.getElementById("winrate3").innerHTML = wins;
-    }else{
-        let nick = document.getElementById("winrate3").getAttribute("data-nick");
-        document.getElementById("winrate3").innerHTML = nick;
-    }
-}
+// function winrate5(){
+//     let str = document.getElementById("winrate5").innerText;
+//     let lastCh = str.charAt(str.length - 1);
+//     if (lastCh != '%'){
+//         let wins = document.getElementById("winrate5").getAttribute("data-winrate");
+//         document.getElementById("winrate5").innerHTML = wins;
+//     }else{
+//         let nick = document.getElementById("winrate5").getAttribute("data-nick");
+//         document.getElementById("winrate5").innerHTML = nick;
+//     }
+// }
 
-function winrate4(){
-    let str = document.getElementById("winrate4").innerText;
-    let lastCh = str.charAt(str.length - 1);
-    if (lastCh != '%'){
-        let wins = document.getElementById("winrate4").getAttribute("data-winrate");
-        document.getElementById("winrate4").innerHTML = wins;
-    }else{
-        let nick = document.getElementById("winrate4").getAttribute("data-nick");
-        document.getElementById("winrate4").innerHTML = nick;
-    }
-}
+// function winrate6(){
+//     let str = document.getElementById("winrate6").innerText;
+//     let lastCh = str.charAt(str.length - 1);
+//     if (lastCh != '%'){
+//         let wins = document.getElementById("winrate6").getAttribute("data-winrate");
+//         document.getElementById("winrate6").innerHTML = wins;
+//     }else{
+//         let nick = document.getElementById("winrate6").getAttribute("data-nick");
+//         document.getElementById("winrate6").innerHTML = nick;
+//     }
+// }
 
-function winrate5(){
-    let str = document.getElementById("winrate5").innerText;
-    let lastCh = str.charAt(str.length - 1);
-    if (lastCh != '%'){
-        let wins = document.getElementById("winrate5").getAttribute("data-winrate");
-        document.getElementById("winrate5").innerHTML = wins;
-    }else{
-        let nick = document.getElementById("winrate5").getAttribute("data-nick");
-        document.getElementById("winrate5").innerHTML = nick;
-    }
-}
+// function winrate7(){
+//     let str = document.getElementById("winrate7").innerText;
+//     let lastCh = str.charAt(str.length - 1);
+//     if (lastCh != '%'){
+//         let wins = document.getElementById("winrate7").getAttribute("data-winrate");
+//         document.getElementById("winrate7").innerHTML = wins;
+//     }else{
+//         let nick = document.getElementById("winrate7").getAttribute("data-nick");
+//         document.getElementById("winrate7").innerHTML = nick;
+//     }
+// }
 
-function winrate6(){
-    let str = document.getElementById("winrate6").innerText;
-    let lastCh = str.charAt(str.length - 1);
-    if (lastCh != '%'){
-        let wins = document.getElementById("winrate6").getAttribute("data-winrate");
-        document.getElementById("winrate6").innerHTML = wins;
-    }else{
-        let nick = document.getElementById("winrate6").getAttribute("data-nick");
-        document.getElementById("winrate6").innerHTML = nick;
-    }
-}
+// function winrate8(){
+//     let str = document.getElementById("winrate8").innerText;
+//     let lastCh = str.charAt(str.length - 1);
+//     if (lastCh != '%'){
+//         let wins = document.getElementById("winrate8").getAttribute("data-winrate");
+//         document.getElementById("winrate8").innerHTML = wins;
+//     }else{
+//         let nick = document.getElementById("winrate8").getAttribute("data-nick");
+//         document.getElementById("winrate8").innerHTML = nick;
+//     }
+// }
 
-function winrate7(){
-    let str = document.getElementById("winrate7").innerText;
-    let lastCh = str.charAt(str.length - 1);
-    if (lastCh != '%'){
-        let wins = document.getElementById("winrate7").getAttribute("data-winrate");
-        document.getElementById("winrate7").innerHTML = wins;
-    }else{
-        let nick = document.getElementById("winrate7").getAttribute("data-nick");
-        document.getElementById("winrate7").innerHTML = nick;
-    }
-}
+// function winrate9(){
+//     let str = document.getElementById("winrate9").innerText;
+//     let lastCh = str.charAt(str.length - 1);
+//     if (lastCh != '%'){
+//         let wins = document.getElementById("winrate9").getAttribute("data-winrate");
+//         document.getElementById("winrate9").innerHTML = wins;
+//     }else{
+//         let nick = document.getElementById("winrate9").getAttribute("data-nick");
+//         document.getElementById("winrate9").innerHTML = nick;
+//     }
+// }
 
-function winrate8(){
-    let str = document.getElementById("winrate8").innerText;
-    let lastCh = str.charAt(str.length - 1);
-    if (lastCh != '%'){
-        let wins = document.getElementById("winrate8").getAttribute("data-winrate");
-        document.getElementById("winrate8").innerHTML = wins;
-    }else{
-        let nick = document.getElementById("winrate8").getAttribute("data-nick");
-        document.getElementById("winrate8").innerHTML = nick;
-    }
-}
-
-function winrate9(){
-    let str = document.getElementById("winrate9").innerText;
-    let lastCh = str.charAt(str.length - 1);
-    if (lastCh != '%'){
-        let wins = document.getElementById("winrate9").getAttribute("data-winrate");
-        document.getElementById("winrate9").innerHTML = wins;
-    }else{
-        let nick = document.getElementById("winrate9").getAttribute("data-nick");
-        document.getElementById("winrate9").innerHTML = nick;
-    }
-}
-
-function winrate10(){
-    let str = document.getElementById("winrate10").innerText;
-    let lastCh = str.charAt(str.length - 1);
-    if (lastCh != '%'){
-        let wins = document.getElementById("winrate10").getAttribute("data-winrate");
-        document.getElementById("winrate10").innerHTML = wins;
-    }else{
-        let nick = document.getElementById("winrate10").getAttribute("data-nick");
-        document.getElementById("winrate10").innerHTML = nick;
-    }
-}
+// function winrate10(){
+//     let str = document.getElementById("winrate10").innerText;
+//     let lastCh = str.charAt(str.length - 1);
+//     if (lastCh != '%'){
+//         let wins = document.getElementById("winrate10").getAttribute("data-winrate");
+//         document.getElementById("winrate10").innerHTML = wins;
+//     }else{
+//         let nick = document.getElementById("winrate10").getAttribute("data-nick");
+//         document.getElementById("winrate10").innerHTML = nick;
+//     }
+// }
